b1550a1969db65616fea43bad0f8ed087b5b6962
Sylvain <kauffeisensylvain@gmail.com>
2017-10-24 18:48:22 +0200

    Peaufinage + bug fix

--- Le glandeur de Bureau/Assets/QTE/GoToQTEScene.cs
+++ Le glandeur de Bureau/Assets/QTE/GoToQTEScene.cs
@@ -8,18 +8,11 @@ public class GoToQTEScene : MonoBehaviour {
 	public GameObject Joueur;
 	private float JoueurPrefX;
 	private float JoueurPrefY;
-	public GameObject Cafe1;
-	public GameObject Cafe2;
-	public GameObject Cafe3;
-	public GameObject Cafe4;
 
 
 	[SerializeField]
 	private static GameObject Enemy1;
-
-	[SerializeField]
-	private string EnemyCourant;
-
+    
 
 	void  Awake(){
 		JoueurPrefX = PlayerPrefs.GetFloat ("Axe X");
@@ -28,21 +21,16 @@ public class GoToQTEScene : MonoBehaviour {
 		}
 
 
-	void PositionJoueur(){
+	void PositionJoueur( string EnemyCourant ) {
 		PlayerPrefs.SetFloat ("Axe X", Joueur.transform.position.x +1.5f);
 		PlayerPrefs.SetFloat ("Axe Y", Joueur.transform.position.y +1.5f);
 		PlayerPrefs.SetInt (EnemyCourant, 0);
-		//Debug.Log (EnemyCourant);
-		//Debug.Log (EnemyCourant[8]);
 
 		if ("C".Equals(EnemyCourant [8].ToString())) {
 			PlayerPrefs.SetInt ("EnemyEnCours", 1);
-			//Debug.Log(PlayerPrefs.GetInt("EnemyEnCours"));
-			//Debug.Log ("Coucou");
 		}
 		if ("R".Equals(EnemyCourant [8].ToString())) {
 			PlayerPrefs.SetInt ("EnemyEnCours", 2);
-			Debug.Log ("Coucou");
 		}
 		if ("I".Equals(EnemyCourant [8].ToString())) {
 			PlayerPrefs.SetInt ("EnemyEnCours", 3);
@@ -59,9 +47,7 @@ public class GoToQTEScene : MonoBehaviour {
 	{
 		if (Enemy.CompareTag("Enemy"))
 		{
-			EnemyCourant = Enemy.gameObject.name;
-			PositionJoueur ();
-
+			PositionJoueur ( Enemy.gameObject.name );
 			SceneManager.LoadScene (1);
 		}
 	}
--- Le glandeur de Bureau/Assets/QTE/StartGame.cs
+++ Le glandeur de Bureau/Assets/QTE/StartGame.cs
@@ -11,64 +11,22 @@ public class StartGame : MonoBehaviour {
 		PlayerPrefs.SetFloat ("Axe X", -7.57f);
 		PlayerPrefs.SetFloat ("Axe Y", -1.06f);
 
-
-		PlayerPrefs.SetInt("CollegueRelou1", 1);
-		PlayerPrefs.SetInt("CollegueRelou2", 1);
-		PlayerPrefs.SetInt("CollegueRelou3", 1);
-		PlayerPrefs.SetInt("CollegueRelou4", 1);
-		PlayerPrefs.SetInt("CollegueRelou5", 1);
-		PlayerPrefs.SetInt("CollegueRelou6", 1);
-		PlayerPrefs.SetInt("CollegueRelou7", 1);
-		PlayerPrefs.SetInt("CollegueRelou8", 1);
-		PlayerPrefs.SetInt("CollegueRelou9", 1);
-		PlayerPrefs.SetInt("CollegueRelou10", 1);
-		PlayerPrefs.SetInt("CollegueRelou11", 1);
-		PlayerPrefs.SetInt("CollegueRelou12", 1);
-		PlayerPrefs.SetInt("CollegueRelou13", 1);
-		PlayerPrefs.SetInt("CollegueRelou14", 1);
-		PlayerPrefs.SetInt("CollegueRelou15", 1);
-		PlayerPrefs.SetInt("CollegueRelou16", 1);
-		PlayerPrefs.SetInt("CollegueRelou17", 1);
-		PlayerPrefs.SetInt("CollegueRelou18", 1);
-		PlayerPrefs.SetInt("CollegueRelou19", 1);
-		PlayerPrefs.SetInt("CollegueRelou20", 1);
-		PlayerPrefs.SetInt("CollegueRelou21", 1);
-		PlayerPrefs.SetInt("CollegueRelou22", 1);
-		PlayerPrefs.SetInt("CollegueRelou23", 1);
-		PlayerPrefs.SetInt("CollegueRelou24", 1);
-		PlayerPrefs.SetInt("CollegueRelou25", 1);
-		PlayerPrefs.SetInt("CollegueRelou26", 1);
-		PlayerPrefs.SetInt("CollegueRelou27", 1);
-		PlayerPrefs.SetInt("CollegueRelou28", 1);
-
-
-		PlayerPrefs.SetInt("CollegueCafe1", 1);
-		PlayerPrefs.SetInt("CollegueCafe2", 1);
-		PlayerPrefs.SetInt("CollegueCafe3", 1);
-		PlayerPrefs.SetInt("CollegueCafe4", 1);
-		PlayerPrefs.SetInt("CollegueCafe5", 1);
-		PlayerPrefs.SetInt("CollegueCafe6", 1);
-		PlayerPrefs.SetInt("CollegueCafe7", 1);
-		PlayerPrefs.SetInt("CollegueCafe8", 1);
-		PlayerPrefs.SetInt("CollegueCafe9", 1);
-		PlayerPrefs.SetInt("CollegueCafe10", 1);
-
-		PlayerPrefs.SetInt("CollegueToilettes1", 1);
-		PlayerPrefs.SetInt("CollegueToilettes2", 1);
-		PlayerPrefs.SetInt("CollegueToilettes3", 1);
-		PlayerPrefs.SetInt("CollegueToilettes4", 1);
-		PlayerPrefs.SetInt("CollegueToilettes5", 1);
-		PlayerPrefs.SetInt("CollegueToilettes6", 1);
-		PlayerPrefs.SetInt("CollegueToilettes7", 1);
-		PlayerPrefs.SetInt("CollegueToilettes8", 1);
-		PlayerPrefs.SetInt("CollegueToilettes9", 1);
-		PlayerPrefs.SetInt("CollegueToilettes10", 1);
-		PlayerPrefs.SetInt("CollegueToilettes11", 1);
-		PlayerPrefs.SetInt("CollegueToilettes12", 1);
-		PlayerPrefs.SetInt("CollegueToilettes13", 1);
-
-
-
+        //CollegueImprimante
+        for ( int i = 0; i < 28; i++ ) {
+            PlayerPrefs.SetInt( "CollegueRelou" + i, 1 );
+        }
+        //CollegueImprimante
+        for ( int i = 0; i < 10; i++ ) {
+            PlayerPrefs.SetInt( "CollegueCafe" + i, 1 );
+        }
+        //CollegueImprimante
+        for ( int i = 0; i < 29; i++ ) {
+            PlayerPrefs.SetInt( "CollegueImprimante" + i, 1 );
+        }
+        //CollegueImprimante
+        for ( int i = 0; i < 14; i++ ) {
+            PlayerPrefs.SetInt( "CollegueToilettes" + i, 1 );
+        }
 	}
 	
 	// Update is called once per frame
