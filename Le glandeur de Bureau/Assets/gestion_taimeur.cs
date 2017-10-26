f79239395b4e97ddf61ee1890a76edd357676ff0
Sylvain <kauffeisensylvain@gmail.com>
2017-10-26 15:33:35 +0200

    Ajout du timer comme condition de défaite

--- Le glandeur de Bureau/Assets/gestion_taimeur.cs
+++ Le glandeur de Bureau/Assets/gestion_taimeur.cs
@@ -5,44 +5,34 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class gestion_taimeur : MonoBehaviour {
+    public float temps = 60;
+    public int maxTime = 120;
+    public  Image timebar;
 
-	[SerializeField]
-	public float temps;
-	[SerializeField]
-	public int maxTime;
-	private Image timebar;
+    void Start() {
+        timebar = GetComponent<Image>();
+    }
+    
+    void Update() {
+        losingTime();
+    }
 
-	void Start ()
-	{
-		timebar = GetComponent<Image>();
-	}
+    // Perd du temps
+    public void losingTime() {
+        temps -= Time.deltaTime;
+        UpdateTime();
+    }
+    // Regagne du temps
+    public void gainTime( float bonus ) {
+        temps += bonus;
+        UpdateTime();
+    }
 
-
-	void Update ()
-	{
-		losingTime ();
-	}
-
-	// Perd du temps
-	public void losingTime()
-	{
-		temps -= Time.deltaTime;
-		UpdateTime();
-	}
-	// Regagne du temps
-	public void gainTime(float bonus)
-	{
-		temps += bonus;
-		UpdateTime();
-	}
-
-	// Actualise le temps pour rester entre 0 et le max
-	private void UpdateTime()
-	{
-		temps = Mathf.Clamp(temps, 0, maxTime);
-		float amount = (float)temps / maxTime;
-		timebar.fillAmount = amount;
-		if (temps == 0)
-			SceneManager.LoadScene(3);
-	}
+    // Actualise le temps pour rester entre 0 et le max
+    private void UpdateTime() {
+        temps = Mathf.Clamp( temps, 0, maxTime );
+        timebar.fillAmount = temps / maxTime;
+        if ( temps == 0 )
+            SceneManager.LoadScene( 2 );
+    }
 }
